f53bb14904132d7c35a09a96c923c81364eb7f60
Paula Lopes <paulalopes@Paulas-MacBook-Air.local>
2021-03-31 21:08:40 -0700

    fuction color

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,3 +7,43 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 });
 
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place + " " + feature.properties.mag +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+  
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    
+    function mag_color(mag){
+      if (mag >4)
+          return "red"
+      else if (mag >3.5)
+          return "orange"
+      else if (mag >3.0)
+          return "yellow"
+      else if (mag >2.5)
+          return "blue"
+      else if (mag >2.0)
+          return "green"
+      else if (mag >1.5)
+          return "lightgreen"
+      else if (mag >1.0)
+          return "gray"
+      else if (mag >0.5)
+          return "black"
+      else
+          return "white"
+  
+  
+    }
+    
+    
+  
+  
+  
+  };
